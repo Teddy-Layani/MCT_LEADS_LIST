17077d2d5891aed3b5646345b854d6f01b111b9f
Shimon Yzhaki <mct1957mct@gmail.com>
2022-12-24 21:02:30 +0000

    First Version to Deploy

--- webapp/controller/Main.controller.js
+++ webapp/controller/Main.controller.js
@@ -1,10 +1,11 @@
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "sap/ui/model/Filter"
 ],
     /**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, Filter) {
         "use strict";
 
         return Controller.extend("zcrmleadslist.controller.Main", {
@@ -16,6 +17,22 @@ sap.ui.define([
                 this.getOwnerComponent().getModel("mainView").setProperty("/leadCount",
                     oEvent.getParameter("total")
                 );
+            },
+
+            onSelectedButtonChange: function(oEvent) {
+                let oSelectedFilter = oEvent.getParameter("item").getBindingContext().getObject(),
+                    oTableBinding   = this.getView().byId("mainLead-table").getBinding("items"),
+                    oModel          = this.getOwnerComponent().getModel(),
+                    aFilters        = [];
+                
+                for (let i = 0; i < oSelectedFilter.TabConditionSet.__list.length; i++) {
+                    const oTabCondition = oModel.getProperty("/" + oSelectedFilter.TabConditionSet.__list[i] );
+                    
+                    aFilters.push( new Filter( "/" + oTabCondition.FieldName, oTabCondition.Operator, oTabCondition.ValueLow));
+                }
+
+                oTableBinding.filter(aFilters);
             }
+
         });
     });
